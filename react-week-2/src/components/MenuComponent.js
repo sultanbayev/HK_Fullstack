7eab5dba590e81e3dd71746fb9c181fe4d3cedcf
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-06-24 02:30:20 +0600

    init week-2

---
+++ react-week-2/src/components/MenuComponent.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
+import DishDetail from "./DishdetailComponent";
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle tag="h5">{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <>
+                <div className="row">
+                    { menu }
+                </div>
+                <DishDetail dish={this.state.selectedDish} />
+            </>
+        );
+    }
+}
+
+export default Menu;
