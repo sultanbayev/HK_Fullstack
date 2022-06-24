de82f3936e8c61d3498474b91406bb4c34781995
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-06-24 16:41:50 +0600

    Single Page Applications Part 2

--- react-week-2/src/components/DishdetailComponent.js
+++ react-week-2/src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({comments}) {
     return (
@@ -31,16 +33,29 @@ function RenderDish({dish}) {
     );
 }
 
-function DishDetail({ dish }) {
+function DishDetail({ dish, comments }) {
     if (dish) {
         return (
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={dish} />
-                </div> 
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={dish.comments} />
                 </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={comments} />
+                </div>
+            </div>
             </div>
         );
     } else {
