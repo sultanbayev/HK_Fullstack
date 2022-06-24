eb15bcd4af5a075822f55eff929bcfe877ea6fe7
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-06-24 15:29:18 +0600

    React Router

--- react-week-2/src/components/MenuComponent.js
+++ react-week-2/src/components/MenuComponent.js
@@ -5,8 +5,7 @@ import { Card, CardImg, CardImgOverlay,
 
 function RenderMenuItem ({dish, onClick}) {
     return (
-        <Card
-            onClick={() => onClick(dish.id)}>
+        <Card>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
@@ -19,7 +18,7 @@ function Menu(props) {
     const menu = props.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         );
     });
